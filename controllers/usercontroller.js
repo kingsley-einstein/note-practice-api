8f4636cef797b461a527445107bb2c1b98388ee2
Kingsley Victor <javaprodigy56@gmail.com>
2018-08-02 19:52:00 +0100

    Added controllers

---
+++ controllers/usercontroller.js
@@ -0,0 +1,14 @@
+module.exports = {
+    register: (req, res) => {
+
+    },
+    login: (req, res) => {
+
+    },
+    setGoal: (req, res) => {
+
+    },
+    deleteGoal: (req, res) => {
+        
+    }
+}
