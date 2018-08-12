86bf53379d768cf2555667dfaea4c4e266d0d3db
Kingsley Victor <javaprodigy56@gmail.com>
2018-08-12 08:53:29 +0100

    Added tests

---
+++ tests/helper.js
@@ -0,0 +1,6 @@
+const sinonchai = require('sinon-chai');
+const chai = require('chai');
+
+global.sinon = require('sinon');
+global.expect = chai.expect;
+chai.use(sinonchai);
---
+++ tests/routes/routes.test.js
@@ -0,0 +1,77 @@
+describe('Route Test', () => {
+    const app = {
+        post: sinon.spy(),
+        use: sinon.spy(),
+        get: sinon.spy(),
+        put: sinon.spy(),
+        post: sinon.spy(),
+        delete: sinon.spy()
+    };
+    require('./../../settings').config(app);
+    require('./../../settings').route(app);
+    beforeEach(() => {
+        console.log('Entering suite');
+    });
+    afterEach(() => {
+        console.log('Test carried out successfully at this point')
+    });
+    describe('Post Calls', () => {
+        it('should call /register', () => {
+            expect(app.post).to.be.calledWith('/register', require('./../../controllers/usercontroller').register);
+        });
+        it('should call /login', () => {
+            expect(app.post).to.be.calledWith('/login', require('./../../controllers/usercontroller').login);
+        });
+        it('should call /:id/newgoal', () => {
+            expect(app.post).to.be.calledWith('/:id/newgoal', require('./../../controllers/usercontroller').setGoal);
+        });
+        it('should call /:id/sendverify', () => {
+            expect(app.post).to.be.calledWith('/:id/sendverify', require('./../../controllers/usercontroller').sendVerification);
+        });
+    });
+    describe('Get Calls', () => {
+        it('should call /:id/:date/:verify', () => {
+            expect(app.get).to.be.calledWith('/:id/:date/verify', require('./../../controllers/usercontroller').verifyUser);
+        });
+        it('should call /:id/pitches', () => {
+            expect(app.get).to.be.calledWith('/:id/pitches', require('./../../controllers/pitchcontroller').displayPitches);
+        });
+        it('should call /:id/:buddyid/share', () => {
+            expect(app.get).to.be.calledWith('/:id/:buddyid/share', require('./../../controllers/usercontroller').shareProgress);
+        });
+        it('should call /global', () => {
+            expect(app.get).to.be.calledWith('/global', require('./../../controllers/usercontroller').displayGlobalRecord);
+        });
+        it('should call /:id', () => {
+            expect(app.get).to.be.calledWith('/:id', require('./../../controllers/usercontroller').displayProfile);
+        });
+        it('should call /:id/:buddyid/request', () => {
+            expect(app.get).to.be.calledWith('/:id/:buddyid/request', require('./../../controllers/usercontroller').sendBuddyRequest);
+        });
+        it('should call /:id/:requestid/accept', () => {
+            expect(app.get).to.be.calledWith('/:id/:requestid/accept', require('./../../controllers/usercontroller').acceptBuddyRequest);
+        });
+        it('should call /:id/:requestid/reject', () => {
+            expect(app.get).to.be.calledWith('/:id/:requestid/reject', require('./../../controllers/usercontroller').rejectBuddyRequest);
+        });
+    });
+    describe('Put Calls', () => {
+        it('should call /:goalid/:id/setprogress', () => {
+            expect(app.put).to.be.calledWith('/:goalid/:id/setprogress', require('./../../controllers/goalcontroller').setProgress);
+        });
+        it('should call /:id/:goalid/setpitch', () => {
+            expect(app.put).to.be.calledWith('/:id/:goalid/setpitch', require('./../../controllers/goalcontroller').setPitch);
+        });
+        it('should call /:id/:goalid/:pitchid/speedup', () => {
+            expect(app.put).to.be.calledWith('/:id/:goalid/:pitchid/speedup', require('./../../controllers/pitchcontroller').setSpeed);
+        });
+    });
+    describe('Delete Calls', () => {
+        it('should call /:id/:goalid/deletegoal', () => {
+            expect(app.delete).to.be.calledWith('/:id/:goalid/deletegoal', require('./../../controllers/usercontroller').deleteGoal);
+        });
+        it('should call /:id/deleteall', () => {
+            expect(app.delete).to.be.calledWith('/:id/deleteall', require('./../../controllers/goalcontroller').deleteEntireGoalRecord);
+        });
+    });
+});
