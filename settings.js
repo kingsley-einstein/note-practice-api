158e36309e2a0141fd3115a8ef4dbcbb30c72260
Kingsley Victor <javaprodigy56@gmail.com>
2018-08-20 22:48:38 +0100

    Edited settings

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ module.exports = {
         app.use(require('morgan')('dev'));
         app.use((req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, token');
             res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
             next();
         });
